8f32aa507c0c5677058b48da03bfb408c10f8525
stephen hedlund <shedlund@chicagobooth.edu>
2023-05-25 15:20:14 +0000

    coffee-app-beginnings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/home", {:controller=> "home", :action=>"home"})
 
+  get("/results", {:controller=> "home", :action=> "results"})
+  
+  get("/playground", {:controller=> "home", :action=> "playground"})
+  
+  get("/", {:controller=> "home", :action=> "login"})
+  
+  get("/signup", {:controller=> "home", :action=> "signup"})
 
 end
