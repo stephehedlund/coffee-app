c1ec4746013f2a209fb7b98df4c23b375f47acb2
stephen hedlund <shedlund@chicagobooth.edu>
2023-05-26 15:58:56 +0000

    added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_25_223435) do
+ActiveRecord::Schema.define(version: 2023_05_26_155802) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "shop_id"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2023_05_25_223435) do
     t.float "latitude"
     t.float "longitude"
     t.float "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
